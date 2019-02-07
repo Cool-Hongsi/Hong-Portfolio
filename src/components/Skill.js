7abd5fe487ab69443027092cb3b8e0c646bfd13f
Sungjun <hongsi140626@gmail.com>
2019-02-07 00:23:50 -0500

    17 commit

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -104,7 +104,7 @@ export default class Skill extends React.Component{
                                 <Col xs="12" sm="6" md="6" lg="6" xl="6">
                                     <Card body className="card">
                                     <CardTitle className="cardTitle">NodeJS (1 Year)</CardTitle>
-                                    <CardText className="cardText">Asynchronous (Promise), Combine DB, MVC Design, REST API, Handling Modules (Socket, HandleBar, etc), Partial Webpack & Babel</CardText>
+                                    <CardText className="cardText">Asynchronous (Promise), Combine DB, MVC Design, REST API, Modules (Socket, HandleBar, etc), Partial Webpack & Babel</CardText>
                                     </Card>
                                 </Col>
                                 <Col xs="12" sm="6" md="6" lg="6" xl="6">
